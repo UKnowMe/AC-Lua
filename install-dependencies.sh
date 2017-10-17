f15e00aca03d416d6fd6de014995b4b9172636e3
IguanaMeow <iguanabg92@gmail.com>
2017-10-17 19:10:11 +0200

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-apt-get install libreadline-dev ncurses-base libpthread-stubs0-dev
+apt-get install build-essential liblua5.1-0-dev libluajit-5.1-dev libreadline-dev libncurses5-dev ncurses-base libpthread-stubs0-dev libenet7
