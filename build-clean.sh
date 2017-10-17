0be46ec8de169f017dd1f21846c15711ca669d6c
IguanaMeow <iguanabg92@gmail.com>
2017-10-17 19:06:16 +0200

    Update build-clean.sh

--- build-clean.sh
+++ build-clean.sh
@@ -1,8 +1,12 @@
+make clean
 cd enet
 make clean
+make distclean
+rm -rf autom4te.cache
 cd ../lua
 make clean
 rm ../lib/liblua.a
-cd ..
+cd ../lanes
 make clean
-rm -r linux_release
+cd ..
+rm -rf linux_release
