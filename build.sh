05d66875828c1adeda8fa2614e39a8a00004fb76
BuildTools <unconfigured@null.spigotmc.org>
2019-05-27 18:54:13 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
+ConcurProc=$(CONCURRENCY_LEVEL=$(getconf _NPROCESSORS_ONLN))
 NumProc=$(($(getconf _NPROCESSORS_ONLN)+1))
 ./build-clean.sh
 cd lanes/src
 ln -sf ../../lua/src lua
 cd ../../enet
 ./configure
-make -j $NumProc
+$ConcurProc make -j $NumProc
 cd ../lua
 make linux install
 cp src/liblua.a ../lib/
 cd ..
-make linux
+$ConcurProc make -j $NumProc linux
